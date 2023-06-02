326cd4874105ea4937e78318a316cf0415df925f
Aleksey Vokhmin <vohmina2011@yandex.ru>
2023-06-02 22:33:42 +0300

    Renamed language

--- get-dictionary.ts
+++ get-dictionary.ts
@@ -2,10 +2,10 @@ import "server-only";
 import type { Locale } from "./i18n-config";
 
 const dictionaries = {
-  en: () => import("./dictionaries/en.json").then((module) => module.default),
+  us: () => import("./dictionaries/us.json").then((module) => module.default),
   ru: () => import("./dictionaries/ru.json").then((module) => module.default),
 };
 
-export type Dictionary = Awaited<ReturnType<typeof dictionaries.en>>;
+export type Dictionary = Awaited<ReturnType<typeof dictionaries.us>>;
 
 export const getDictionary = async (locale: Locale) => dictionaries[locale]();
--- i18n-config.ts
+++ i18n-config.ts
@@ -1,6 +1,6 @@
 export const i18n = {
   defaultLocale: "ru",
-  locales: ["en", "ru"],
+  locales: ["us", "ru"],
 } as const;
 
 export type Locale = (typeof i18n)["locales"][number];
