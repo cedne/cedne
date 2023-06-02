307c9367da0571a8f5080aea1eba53e8440ecc74
Aleksey Vokhmin <vohmina2011@yandex.ru>
2023-06-02 22:34:46 +0300

    Changed switcher and added it to layout

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -3,6 +3,7 @@ import { Locale, i18n } from "@/i18n-config";
 import type { Metadata } from "next";
 import "./globals.css";
 import Footer from "./footer";
+import LocaleSwitcher from "./locale-switcher";
 
 export async function generateStaticParams() {
   return i18n.locales.map((locale) => ({ lang: locale }));
@@ -32,7 +33,11 @@ export default async function Root({
 
   return (
     <html lang={params.lang}>
-      <body>
+      <body className="relative">
+        <LocaleSwitcher
+          locale={params.lang}
+          className="absolute z-10 top-4 right-4"
+        />
         {children}
         <Footer dict={dict.footer} />
       </body>
--- app/[lang]/locale-switcher.tsx
+++ app/[lang]/locale-switcher.tsx
@@ -2,9 +2,25 @@
 
 import { usePathname } from "next/navigation";
 import Link from "next/link";
-import { i18n } from "@/i18n-config";
+import { Locale, i18n } from "@/i18n-config";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuRadioGroup,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { DropdownMenuRadioItem } from "@radix-ui/react-dropdown-menu";
+import { useEffect, useState } from "react";
+import { Skeleton } from "@/components/ui/skeleton";
 
-export default function LocaleSwitcher() {
+export default function LocaleSwitcher({
+  locale,
+  ...props
+}: { locale: Locale } & React.ComponentPropsWithoutRef<"div">) {
   const pathName = usePathname();
   const redirectedPathName = (locale: string) => {
     if (!pathName) return "/";
@@ -12,19 +28,86 @@ export default function LocaleSwitcher() {
     segments[1] = locale;
     return segments.join("/");
   };
+  const locales = () => {
+    return i18n.locales.map((locale) => {
+      return {
+        locale,
+        flag: import(
+          `../../node_modules/country-flag-icons/3x2/${locale.toUpperCase()}.svg`
+        ).then((module) => module.default) as Promise<{ src: string }>,
+      };
+    });
+  };
+  const [localeFlags, setLocaleFlags] = useState<
+    {
+      locale: Locale;
+      flag: Awaited<ReturnType<typeof locales>[number]["flag"]>;
+    }[]
+  >([]);
+
+  useEffect(() => {
+    locales().map((loc) => {
+      loc.flag.then((img) => {
+        setLocaleFlags((localeFlags) => {
+          if (
+            localeFlags?.find((localeFlag) => localeFlag.locale === loc.locale)
+          )
+            return localeFlags;
+          return [...localeFlags, { locale: loc.locale, flag: img }];
+        });
+      });
+    });
+  }, []);
+
+  if (localeFlags.length === 0)
+    return (
+      <div {...props}>
+        <Skeleton className="w-10 h-10 rounded-full" />
+      </div>
+    );
 
   return (
-    <div>
-      <p>Locale switcher:</p>
-      <ul>
-        {i18n.locales.map((locale) => {
-          return (
-            <li key={locale}>
-              <Link href={redirectedPathName(locale)}>{locale}</Link>
-            </li>
-          );
-        })}
-      </ul>
+    <div {...props}>
+      <DropdownMenu modal={false}>
+        <DropdownMenuTrigger>
+          <Avatar>
+            <AvatarFallback>{locale}</AvatarFallback>
+            <AvatarImage
+              src={
+                localeFlags.find((localeFlag) => localeFlag.locale === locale)
+                  ?.flag.src ?? ""
+              }
+              alt={locale}
+            />
+          </Avatar>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="min-w-[3rem] text-center">
+          <DropdownMenuRadioGroup>
+            {i18n.locales.map((locale, index) => {
+              return (
+                <>
+                  <DropdownMenuRadioItem
+                    key={locale}
+                    value={locale}
+                    className="min-w-min"
+                  >
+                    <Link
+                      href={redirectedPathName(locale)}
+                      className="block hover:bg-gray-700 transition-colors duration-200
+                  "
+                    >
+                      {locale}
+                    </Link>
+                  </DropdownMenuRadioItem>
+                  {index < i18n.locales.length - 1 && (
+                    <DropdownMenuSeparator key={index} />
+                  )}
+                </>
+              );
+            })}
+          </DropdownMenuRadioGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
   );
 }
