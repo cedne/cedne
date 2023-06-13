dbd7996e957d024b6ec4cb2d719e8050d0c439ec
Aleksey Vokhmin <vohmina2011@yandex.ru>
2023-06-14 00:54:24 +0300

    Removed assests API

--- middleware.ts
+++ middleware.ts
@@ -44,7 +44,5 @@ export function middleware(request: NextRequest) {
 
 export const config = {
   // Matcher ignoring `/_next/` and `/api/`
-  matcher: [
-    "/((?!api|_next/static|_next/image|favicon.ico|assets|create-record).*)",
-  ],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|create-record).*)"],
 };
