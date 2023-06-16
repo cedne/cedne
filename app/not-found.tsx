ae81dfb1813b39efd33e4bfcdc01cdbc673e88d6
Aleksey Vokhmin <vohmina2011@yandex.ru>
2023-06-16 17:22:31 +0300

    Fixed 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,10 +1,8 @@
 export default function NotFound() {
   return (
-    <html>
-      <body>
-        <h1>404</h1>
-        <p>Not found</p>
-      </body>
-    </html>
+    <div className="flex flex-col items-center justify-center h-screen text-4xl text-gray-700">
+      <h1>404</h1>
+      <p>Not found</p>
+    </div>
   );
 }
